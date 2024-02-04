9aac5179408cd9637203f35798c1885b68b27fa8
Erce Uslu <erceonline@gmail.com>
2024-02-05 00:22:15 +0300

    Game Server Push

---
+++ GameServer/Server.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class Server
+    {
+        public static int maxPlayers { get; private set; }
+        public static int port { get; private set; }
+        public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
+        private static TcpListener tcpListener;
+        public delegate void PacketHandler(int _fromClient, Packet _packet);
+        public static Dictionary<int, PacketHandler> packetHandlers;
+        
+        public static void Start(int _maxPlayers, int _port)
+        {
+            maxPlayers = _maxPlayers;
+            port = _port;
+            InitializeServerData();
+            tcpListener = new TcpListener(IPAddress.Any, port);
+            tcpListener.Start();
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+
+            Console.WriteLine("*****************************Server started! Port: " + port + "*****************************");
+        }
+
+        private static void TCPConnectCallback(IAsyncResult _result)
+        {
+            TcpClient _client = tcpListener.EndAcceptTcpClient(_result);
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+            Console.WriteLine($"Incoming connection from {_client.Client.RemoteEndPoint}...");
+            for (int i = 0; i < maxPlayers; i++)
+            {
+                if (clients[i].tcp.socket == null)
+                {
+                    clients[i].tcp.Connect(_client);
+                    return;
+                }
+            }
+        }
+
+        private static void InitializeServerData()
+        {
+            for (int i = 0; i < maxPlayers; i++)
+            {
+                clients.Add(i, new Client(i));
+            }
+            packetHandlers = new Dictionary<int, PacketHandler>()
+            {
+                {(int)ClientPackets.welcomeReceived, ServerHandle.WelcomeReceived }
+            };
+            Console.WriteLine("Initialize packets.");
+        }
+    }
+}
